cc32d424199e9d07a905da90b2a6a3166194e9e5
Christian Chapman-Bird <c.chapman-bird.1@research.gla.ac.uk>
2022-12-05 14:50:57 +0000

    Reformatting and removing prints

--- hierarchical_flows/likelihood.py
+++ hierarchical_flows/likelihood.py
@@ -39,18 +39,10 @@ class FlowLikelihood:
         rescaled_conditional = self.conditional_rescaling_function(conditional)
         if rescaled_conditional.dim() == 1:
             rescaled_conditional = rescaled_conditional.unsqueeze(0)
-        # conditional has shape Npoints * Nconparams
-        # data has shape Nevents * Nsamples * Nparams
-        # we want to vectorise over both of these, producing Npoints log-likelihoods
-        # logsumexp over each event's probability, then sum over logs for total log-likelihoods
-
-        # the flow takes in 2d
-
         nevents, nsamples, nparams = data.shape
         npoints, nconparams = rescaled_conditional.shape
         if self.batch_size is None:  # do all in one
             data_in = data.unsqueeze(0).expand(npoints, -1, -1, -1).reshape(-1, nparams)
-            # cond_in = rescaled_conditional.unsqueeze(2).unsqueeze(3).expand(-1, -1, nevents, nsamples).reshape(-1, nconparams)
             cond_in = (
                 rescaled_conditional.unsqueeze(1)
                 .expand(-1, nevents * nsamples, -1)
@@ -66,7 +58,6 @@ class FlowLikelihood:
         else:
             log_prob = torch.zeros(npoints)
             nbatches = np.ceil(npoints / self.batch_size).astype(np.int32)
-            # cond_in = rescaled_conditional.unsqueeze(2).unsqueeze(3).expand(-1, -1, nevents, nsamples)
             cond_in = rescaled_conditional.unsqueeze(1).expand(
                 -1, nevents * nsamples, -1
             )
@@ -74,22 +65,19 @@ class FlowLikelihood:
             for k in range(nbatches):
                 this_cond = cond_in[
                     k * self.batch_size : min((k + 1) * self.batch_size, npoints), :, :
-                ]  # ,:]
+                ]
                 data_in = (
                     data.unsqueeze(0)
                     .expand(this_cond.shape[0], -1, -1, -1)
                     .reshape(-1, nparams)
                 )
 
-                # print(data_in.shape, cond_in.shape)
                 with torch.no_grad():
-                    # print('here')
                     probs = self.flow.log_prob(
                         data_in, conditional=this_cond.reshape(-1, nconparams)
                     ).reshape(this_cond.shape[0], nevents, nsamples)
                     log_prob[
                         k * self.batch_size : min((k + 1) * self.batch_size, npoints)
                     ] = torch.sum(torch.logsumexp(probs, dim=-1), dim=-1)
-                    # print('done')
         log_prob -= np.log(nsamples) * nevents
         return log_prob
