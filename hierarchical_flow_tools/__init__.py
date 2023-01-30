ada06a03951c1d56b389ccc47e3a4c183989311a
Christian Chapman-Bird <c.chapman-bird.1@research.gla.ac.uk>
2023-01-30 17:52:07 +0000

    fixed version

--- hierarchical_flow_tools/__init__.py
+++ hierarchical_flow_tools/__init__.py
@@ -2,3 +2,11 @@
 hierarchical-flow-tools is a lightweight package for easily performing fully vectorised hierarchical inference with pre-trained normalising flows.
 """
 from .likelihood import FlowLikelihood
+
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = "unknown"
